847cadffcba8b0c1d0397a8df185a82fdf60319a
Dapoy <farisdenis8@gmail.com>
2025-08-19 10:14:09 +0700

    [Not Done] Membuat halaman panel admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard Admin')
+
+@section('content_header')
+    <h1>Dashboard Admin</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>Peminjaman</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-book"></i>
+                </div>
+                <a href="#" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
-use App\Http\Controllers\AssetController;
-use App\Http\Controllers\PeminjamController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PeminjamController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 });
 
 Route::get('/dashboard', function () {
@@ -19,7 +24,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/daftar', [AssetController::class, 'index'])->name('assets.index');
     Route::post('/peminjam', [PeminjamController::class, 'store'])->name('peminjam.store');
-
 });
 
 require __DIR__.'/auth.php';
